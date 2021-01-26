ff84ac0b1edace83e1d4890d9a66b1d4fab6acf8
HasanNaser <19hasan0@gmail.com>
2021-01-26 20:54:10 +0300

    fix bugs

--- opera_extension/main.js
+++ opera_extension/main.js
@@ -4,12 +4,11 @@ var justDoIt = function (){
     let noti_element = document.querySelector(".notification-button-style-type-default");
     (noti_element) ? noti_element.remove() : null;
 
-    let searchbar_element = document.querySelector("#search-form");
-    searchbar_element.style.border="5px solid red";
-    searchbar_element.style.caretColor ="red";
-    searchbar_element.parentElement.parentElement.removeAttribute("class");
-    searchbar_element.parentElement.parentElement.style.width="60%";
-
+    // let searchbar_element = document.querySelector("#search-form");
+    // searchbar_element.style.border="5px solid red";
+    // searchbar_element.style.caretColor ="red";
+    // searchbar_element.parentElement.parentElement.removeAttribute("class");
+    // searchbar_element.parentElement.parentElement.style.width="60%";
 
     if (window.location.href.indexOf("watch?v=") < 0) {
         let recommended_element = document.querySelector("#dismissable")
@@ -24,16 +23,23 @@ var justDoIt = function (){
         }
     } else {
         let related_element = document.querySelector("#related")
-        let comments_element = document.querySelector("#comments")
-
+        // let comments_element = document.querySelector("#comments")
         if(related_element){
-            related_element.parentElement.parentElement.remove();
+            related_element.parentElement.parentElement.style.display= "none";
         }
-        if(comments_element){
-            comments_element.style.display = "none";
-        }
+        // if(comments_element){
+        //     comments_element.style.display = "none";
+        // }
     }
+
+
+
+
+
+
+
 }
+
 
 chrome.storage.local.get('youtube_reccomendation_durum', function(data) {
     if(data.youtube_reccomendation_durum === "yes")
@@ -41,7 +47,7 @@ chrome.storage.local.get('youtube_reccomendation_durum', function(data) {
             justDoIt()
             setInterval(function () {
                 justDoIt()
-            }, 500);
+            }, 400);
         }
 
 });
